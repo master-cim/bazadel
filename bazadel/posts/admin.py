1248f3ba88cd1de250b249d9d4f8d89405826925
Svetlana <master@sevsu.energynet.ru>
2023-10-18 16:06:26 +0300

    модель и админка

--- bazadel/posts/admin.py
+++ bazadel/posts/admin.py
@@ -1,3 +1,93 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import Service, Status, Order, Project, Workflow
+
+admin.site.unregister(User)
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = UserCreationForm.Meta.model
+        fields = '__all__'
+        field_classes = UserCreationForm.Meta.field_classes
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    add_form = CustomUserCreationForm
+    add_fieldsets = (
+        (None, {'fields': ('username', 'password1', 'password2')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups',
+                       'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff',
+                    'date_joined',)
+    search_fields = ('email', 'first_name', 'last_name')
+    list_filter = ('date_joined',)
+    empty_value_display = '-пусто-'
+    ordering = ('email',)
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ('pk', 'title',
+                    'description',)
+    search_fields = ('title',)
+    list_filter = ('slug',)
+    empty_value_display = '-пусто-'
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'author', 'service',
+                    'text', 'phone_number', 'e_mail',
+                    'pub_date',)
+    search_fields = ('author', 'phone_number', 'e_mail',
+                    'pub_date',)
+    list_filter = ('author', 'service', 'pub_date',)
+    empty_value_display = '-пусто-'
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'description',
+                    'prev_status',)
+    search_fields = ('prev_status',)
+    list_filter = ('name', )
+    empty_value_display = '-пусто-'
+
+
+class MembershipInline(admin.TabularInline):
+    model = Project.status.through
+
+
+class WorkflowAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'status', 'project',
+                    'start_date', 'end_date',
+                    'process_mng')
+    search_fields = ('prev_status',)
+    list_filter = ('status',  'project','start_date', 'end_date',
+                    'process_mng')
+    empty_value_display = '-пусто-'
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [
+        MembershipInline,
+    ]
+    exclude = ('status',)
+
+
+
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Workflow, WorkflowAdmin)
--- bazadel/posts/models.py
+++ bazadel/posts/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.core.validators import RegexValidator
+from django.db.models import Q, F
 
 
 User = get_user_model()
 
 
 class Service(models.Model):
-    title = models.CharField(max_length=200, unique=True)
+    title = models.CharField(max_length=200,)
     slug = models.SlugField(unique=True)
     description = models.TextField()
 
@@ -15,27 +16,59 @@ class Service(models.Model):
         return self.title
 
 
-class Order(models.Model):
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='serviсes', verbose_name='Заказчик услуги'
+class Status(models.Model):
+    PREPARATION = 'INITIAL'
+    SPECIFICATION = 'TZ'
+    PROJECT = 'PROJECT'
+    DESIGN = 'DESIGN'
+    DEVELOPING = 'DEVELOP'
+    TESTING = 'TEST'
+    STARTUP = 'STARTUP'
+    MAINTAIN ='MAINTAIN'
+    WAIT = 'WAIT'
+    START = 'START'
+    FINISH = 'FINISH'
+ 
+    STATUS_TYPES = (
+        (WAIT, 'Ожидание'),
+        (START, 'Старт'),
+        (PREPARATION, 'Интервью'),
+        (SPECIFICATION, 'Техническое задание'),
+        (PROJECT, 'Проект'),
+        (DESIGN, 'Дезайн'),
+        (DEVELOPING, 'Разработка'),
+        (TESTING, 'Тестирование'),
+        (STARTUP, 'Запуск продукта'),
+        (MAINTAIN, 'Техническая поддержка'),
+        (FINISH, 'Проект закрыт')
     )
+    
+    name = models.CharField(max_length=21, choices=STATUS_TYPES)
+    description = models.TextField()
+    prev_status = models.ForeignKey('self', null=True, blank=True, related_name='status', on_delete = models.SET_NULL, )
+    
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    author = models.CharField(max_length=100)
     service = models.ForeignKey(
         'Service',
         on_delete=models.SET_NULL,
         related_name='types',
-        null=True,
+        null=True, blank=True,
         verbose_name='Вид услуги',
         help_text='Выберите вид услуги'
     )
     text = models.TextField('Текст заявки',
                             help_text='Введите текст заявки')
     phone_regex = RegexValidator(regex=r'^\+?7?\d{10}$', message="Телефонный номер должен быть введен в формате: '+799999999'. Допускается не более 11 цифр.")
-    phone_number = models.CharField(validators=[phone_regex], max_length=12, blank=False, null=False,)
+    phone_number = models.CharField(validators=[phone_regex], max_length=12,)
     e_mail = models.EmailField(verbose_name='Электронный адрес',
                                max_length=200,
-                               unique=True)
+                               )
     pub_date = models.DateTimeField('Дата заявки',
                                     auto_now_add=True)
     
@@ -44,8 +77,54 @@ class Order(models.Model):
         ordering = ('-pub_date',)
 
     def __str__(self):
-        return self.text[:15]
+        return self.author + " " + self.text[:15]
 
 
 
     
+class Project(models.Model):
+    title = models.CharField(max_length=250)
+    customer = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='project', verbose_name='Заказчик услуги'
+    )
+    order = models.ForeignKey('Order', on_delete=models.CASCADE,
+                              related_name='order', verbose_name='Заказ', 
+                              help_text='Выберите заказ', null=True, blank=True,)
+    status = models.ManyToManyField(Status, through="Workflow")
+    project_mng = models.ForeignKey(User, on_delete=models.SET_NULL,
+                               related_name='project_mng',
+                               null=True, blank=True,)
+    start_date = models.DateTimeField('Дата заявки',
+                                    auto_now_add=True)
+    
+
+    class Meta:
+        ordering = ('-start_date',)
+
+    def __str__(self):
+        return self.title[:15]
+
+
+class Workflow(models.Model):
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    start_date = models.DateTimeField('Дата начала этапа',
+                                    auto_now_add=True)
+    end_date = models.DateTimeField('Дата окончания этапа',
+                                    null=True,
+                               blank=True,)
+    process_mng = models.ForeignKey(User, on_delete=models.SET_NULL,
+                               related_name='process_mng',
+                               null=True,
+                               blank=True,)
+    
+    class Meta:
+        ordering = ('-project',)
+        constraints = [
+            models.CheckConstraint(
+                name='start_date_before_end_date',
+                check=Q(start_date__lt=F('end_date'))
+            )
+        ]
